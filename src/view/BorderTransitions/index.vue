3d95a8cfe85e156a794acbc602a2c78350298381
LuminaQAQ <53186053+LuminaQAQ@users.noreply.github.com>
2024-09-16 17:42:37 +0800

    feat: 添加了一些过渡动画

--- src/view/BorderTransitions/index.vue
+++ src/view/BorderTransitions/index.vue
@@ -61,9 +61,25 @@ import SgCard from "../../components/SgCard.vue";
 const codes = {};
 
 Object.assign(codes, {
-  fill: {
+  borderFade: {
     title: "填充",
-    className: "fill",
+    className: "borderFade",
+    code: `
+.borderFade {
+    box-sizing: border-box;
+    border: 3px solid transparent;
+
+    transition: border-color .3s;
+}
+
+.borderFade:hover {
+    border-color: #409EFF;
+}
+`,
+  },
+  hollow: {
+    title: "空心",
+    className: "hollow",
     code: `
 .fill {
     transition: background-color .3s, color .3s;
@@ -72,6 +88,250 @@ Object.assign(codes, {
 .fill:hover {
     background-color: #409EFF;
     color: white;
+}
+`,
+  },
+  trim: {
+    title: "镂空",
+    className: "trim",
+    code: `
+.trim {
+    position: relative;
+    box-sizing: border-box;
+    perspective: 1px;
+
+}
+
+.trim::after {
+    content: '';
+    display: block;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+
+    width: calc(100% - 12px);
+    height: calc(100% - 12px);
+
+    border: 3px solid transparent;
+
+    z-index: -1;
+
+    transition: border-color .3s;
+}
+
+.trim:hover::after {
+    border-color: #ffffff;
+}
+`,
+  },
+  rippleOut: {
+    title: "涟漪-散开",
+    className: "rippleOut",
+    code: `
+.rippleOut {
+    position: relative;
+    box-sizing: border-box;
+    perspective: 1px;
+
+    outline: 3px solid;
+    outline-offset: 0;
+    outline-color: transparent;
+}
+
+.rippleOut:hover {
+    animation: rippleOut .75s;
+}
+
+@keyframes rippleOut {
+    from {
+        outline-color: #ebeef5;
+    }
+
+    to {
+        outline-color: transparent;
+        outline-offset: 5px;
+    }
+}
+`,
+  },
+  rippleIn: {
+    title: "涟漪-聚拢",
+    className: "rippleIn",
+    code: `
+.rippleIn {
+    position: relative;
+    box-sizing: border-box;
+    perspective: 1px;
+
+    outline: 5px solid;
+    outline-color: transparent;
+}
+
+.rippleIn:hover {
+    animation: rippleIn 1s;
+}
+
+@keyframes rippleIn {
+    from {
+        outline-color: transparent;
+        outline-offset: 15px;
+    }
+
+    to {
+        outline-color: #ebeef5;
+        outline-offset: -5px;
+    }
+}
+`,
+  },
+  outlineOut: {
+    title: "轮廓-展开",
+    className: "outlineOut",
+    code: `
+.outlineOut {
+    position: relative;
+    box-sizing: border-box;
+    perspective: 1px;
+
+    outline: 5px solid #ebeef5;
+    outline-offset: 0;
+
+    transition: outline-offset .5s;
+}
+
+.outlineOut:hover {
+    outline-offset: 10px;
+}
+`,
+  },
+  outlineIn: {
+    title: "轮廓-聚拢",
+    className: "outlineIn",
+    code: `
+.outlineIn {
+    position: relative;
+    box-sizing: border-box;
+    perspective: 1px;
+
+    outline: 5px solid transparent;
+}
+
+.outlineIn:hover {
+    animation: outlineIn .5s forwards;
+}
+
+@keyframes outlineIn {
+    0% {
+        outline-offset: 15px;
+    }
+
+    100% {
+        outline-color: #ebeef5;
+        outline-offset: 10px;
+    }
+}
+`,
+  },
+  roundCorners: {
+    title: "圆角",
+    className: "roundCorners",
+    code: `
+.roundCorners {
+    position: relative;
+    box-sizing: border-box;
+
+    transition: border-radius .3s;
+}
+
+.roundCorners:hover {
+    border-radius: 8px;
+}
+`,
+  },
+  underlineFromLeft: {
+    title: "下划线-从左到右",
+    className: "underlineFromLeft",
+    code: `
+.underlineFromLeft {
+    position: relative;
+    box-sizing: border-box;
+}
+
+.underlineFromLeft::after {
+    content: '';
+    display: block;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+
+    height: 5px;
+    width: 0;
+    background-color: #409EFF;
+
+    transition: width .3s;
+}
+
+.underlineFromLeft:hover::after {
+    width: 100%;
+}
+`,
+  },
+  underlineFromCenter: {
+    title: "下划线-从中间展开",
+    className: "underlineFromCenter",
+    code: `
+.underlineFromCenter {
+    position: relative;
+    box-sizing: border-box;
+}
+
+.underlineFromCenter::after {
+    content: '';
+    display: block;
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+
+    transform: translateX(-50%);
+
+    height: 5px;
+    width: 0;
+    background-color: #409EFF;
+
+    transition: width .3s;
+}
+
+.underlineFromCenter:hover::after {
+    width: 100%;
+}
+`,
+  },
+  underlineFromRight: {
+    title: "下划线-从右到左",
+    className: "underlineFromRight",
+    code: `
+.underlineFromRight {
+    position: relative;
+    box-sizing: border-box;
+}
+
+.underlineFromRight::after {
+    content: '';
+    display: block;
+    position: absolute;
+    bottom: 0;
+    right: 0;
+
+    height: 5px;
+    width: 0;
+    background-color: #409EFF;
+
+    transition: width .3s;
+}
+
+.underlineFromRight:hover::after {
+    width: 100%;
 }
 `,
   },
